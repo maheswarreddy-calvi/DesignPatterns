b9520501c2d1d31f69731d6ff4bf7ea81ded5885
maheswarreddy-calvi <69466570+maheswarreddy-calvi@users.noreply.github.com>
2022-05-10 10:23:52 +0530

    singleton pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns;
+
 Console.WriteLine("Hello, World!");
+SingletonDesignPattern obj = SingletonDesignPattern.getInstance();
+obj.Print();
---
+++ DesignPatterns/SingletonDesignPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class SingletonDesignPattern
+    {
+        private static int id = 0;
+        private static SingletonDesignPattern obj;
+
+        private SingletonDesignPattern()
+        {
+            id++;
+        }
+
+        public static SingletonDesignPattern getInstance()
+        {
+            if (obj == null)
+            {
+                obj = new SingletonDesignPattern();
+            }
+            return obj;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Value {id}");
+        }
+    }
+}
